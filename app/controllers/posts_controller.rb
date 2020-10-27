f1a4b7c298b2178b1f7191e8d0496fb058f0a488
nohaderf <freda.hon@gmail.com>
2020-10-27 12:02:25 -0400

    commit tree a043df8633db40d6258a7054af3a4dadc1842479

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,14 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save 
+    redirect_to post_path(@post) #redirect that leverages a route helper method
+    # redirecting to the new resource's show page
+  end
+
 end
